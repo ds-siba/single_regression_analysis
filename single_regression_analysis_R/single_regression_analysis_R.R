9cc08f76a67d622fa0629976757eb13e48917a53
halnatsu <masanori5602@gmail.com>
2023-05-13 00:12:47 +0900

    単回帰分析

---
+++ single_regression_analysis_R/single_regression_analysis_R.R
@@ -0,0 +1,19 @@
+# csvファイルの読み込み
+df <- read.csv("data/advertising_data.csv", fileEncoding ="cp932")
+
+# 散布図
+X = df$ad
+y = df$sales
+plot(X, y, xlab="ad", ylab="sales")
+
+# 相関係数の確認
+cor(X, y)
+
+# 最小二乗法モデルを作成
+model <- lm(y ~ X)
+
+# 回帰係数と切片を表示
+coef(model)
+
+# グラフ
+abline(model)
